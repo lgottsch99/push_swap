e8b524f430015d4101f99fabcc7957d1f9751cf6
lgottsch99 <lgottsch@student.42prague.com>
2024-12-31 17:21:46 +0100

    dec 31

--- bigger_sort.c
+++ bigger_sort.c
@@ -9,24 +9,79 @@ t_list	*get_node_at_pos(t_list *list, int pos)
 
 	node = list;
 	i = 1;
-	while (i != pos)
-		node = node->next;
-	
+	while (i != pos && pos <= ft_lstsize(list))
+	{
+		if (node->next)
+			node = node->next;
+		else
+			break ;
+	}
 	return (node);
 }
 
-int	get_pos_in_b(t_list	*node_a, t_list	*b, int max_b)
+int	get_pos_in_b(t_list	*node_a, t_list	*b, t_list	*min)//we know safe dass new < max!
 {
-	//b is sorted descending
-	//traverse list b and find correct pos for node_a (not min or max)
+	//einfach von anfang bis ende durchgehen!
 	int	pos;
+	t_list	*tmp;
+	t_list	*next;
 
-	pos = 1;
-	//start at max and then go down until *(int *)node_a ???is what
-	//TO DO ;
-	//how to find correct pos in b for random node
+	pos = 0;
+	tmp = min; //start at min
+	if (tmp->next)
+		next = tmp->next;
+	else
+		next = b; //falls am ende start am anfang
 
+	//go to minpos, then go further until next is > than new
+	while (pos == 0)//only change pos if found
+	{
+		//compare tmp + next to new
+		if ((*(int *)tmp->content < *(int *)node_a->content) && (*(int *)next->content > *(int *)node_a->content))
+			pos = get_position(b, tmp);
+		else
+		{
+			if (tmp->next)
+				tmp = tmp->next;
+			else
+				tmp = b;
+			if (next->next)
+				next = next->next;
+			else
+				next = b;
+		}
+	}
+	ft_printf("pos für new in b: %i\n", pos);
+	return (pos);
+}
 
+int calc_steps(t_list *lst, int pos) //pos needs to be 1 to push correctly
+{
+	int	steps;
+	int	mid;
+	int size;
+
+	size = ft_lstsize(lst) / 2;
+	mid = size / 2;
+	steps = 0;
+
+	if (pos <= mid)//in erster hälfte
+	{
+		while (pos != 1)
+		{
+			pos--;
+			steps++;
+		}
+	}
+	else if (pos > mid)//zweite hälfte
+	{
+		while (pos <= (size + 1))
+		{
+			pos++;
+			steps++;
+		}
+	}
+	return (steps);
 }
 
 int	rotation_b(t_list *node_a, t_list *b)
@@ -43,6 +98,8 @@ int	rotation_b(t_list *node_a, t_list *b)
 	int		steps;
 	
 	//get size of list b, max pos and minpos
+	steps = 0;
+
 	size_b = ft_lstsize(b);
 	min = get_min(b);
 	max = get_max(b);
@@ -77,10 +134,11 @@ int	rotation_b(t_list *node_a, t_list *b)
 	else//new is inbetween
 	{
 		//traverse list b and find correct pos for node_a
-		pos_in_b = get_pos_in_b(node_a, b, maxpos);
+		pos_in_b = get_pos_in_b(node_a, b, min);
 		//calc steps how to get there (rotation only)
+		steps += calc_steps(b, pos_in_b);
 	}
-		ft_printf("rotation steps b: %i\n", steps);
+	ft_printf("rotation steps b: %i\n", steps);
 	return (steps);
 
 }
@@ -133,25 +191,34 @@ t_stack	*do_bigger_sort(t_stack	*stacks)
 	print_both(stacks);
 
 	size_a = ft_lstsize(stacks->a);
-	
 	pos_a = 1;
 	//cheapest = pos_a;
 
+	//TO DO: some structure to keep track of results??
 	while (pos_a <= size_a)
 	{
+		ft_printf("\nchecking stack a pos: %i\n", pos_a);
+
 		steps_needed = 0;
 	//go thru whole stack a (until only 3 left) and calc for each number how many steps (in a and b) would be needed to put in right position
 		//always: get min and max in b, compare each new number to this
 			//if > max oder <min dann pos eindeutig (rotate until true und pb)
 			//if dazwischen: get position of closest 
 		node = get_node_at_pos(stacks->a, pos_a);
+		ft_printf("got node at pos\n");
+
 	//1. pb nur möglich wenn node pos 1 in a
 		//get position in a, decide which way to rotate: ra oder rra
 		steps_needed += rotation_a(pos_a, size_a);
+				ft_printf("got rot a\n");
+
 	//2. check conditions in b: (und size b? bei 2 eig egal)
 		//node in a > maxb oder < minb
 		//if dazwischen: get position of closest 
 		steps_needed += rotation_b(node, stacks->b);
+						ft_printf("got rot b\n");
+
+	//TO DO: rotation check: if same direction check if ss rrr etc possible!
 
 	ft_printf("pos: %i\n",pos_a);
 	ft_printf("total steps_needed: %i\n", steps_needed);
@@ -167,7 +234,7 @@ t_stack	*do_bigger_sort(t_stack	*stacks)
 	}
 	pos_a++;
 	}
-	ft_printf("cheapest pos a: %i\n", cheapest_pos);
+	ft_printf("\ncheapest pos in a: %i\n", cheapest_pos);
 
 
 	return (stacks);
